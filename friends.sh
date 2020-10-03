71c9720480127bf322569d1714ee976af6bed42b
ahmedmlih <ahmedmlih27@gmail.com>
2020-10-03 20:40:42 +0200

    Premier exercice terminé

---
+++ friends.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if (($# == 0));
+then 
+	echo "You have no friend."
+elif (($# == 1));
+then
+	echo "You have 1 friend : $@."
+elif (($# > 1));
+then 
+	names="$1"
+	for ((i=2; i<=$#; i++));
+	do 
+		names="$names, ${!i}"
+	done
+	echo "you have $# friends : $names."
+fi 
+exit $#
+
